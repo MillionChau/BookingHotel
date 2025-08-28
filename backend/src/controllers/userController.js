5b9d52719f519bee0bb75886be6b1f5c443f327e
MillionChau <chaumillions@gmail.com>
2025-08-28 18:47:56 +0700

    add some routes of user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -8,31 +8,35 @@ const changeUserPassword = async (req, res) => {
 
     try {
         const user = await User.findOne({ userId })
+        if (!user) {
+            return res.status(404).json({ message: 'Không tìm thấy người dùng!' })
+        }
+        
         const isPasswordValid = await bcrypt.compare(password, user.password)
-
-        if(isPasswordValid !== password)
-            return res.status(401).json({ message: 'Mật khẩu không đúng!'})
-
+        
+        if(!isPasswordValid)
+            return res.status(401).json({ message: 'Mật khẩu không đúng!' })
+       
         if(newPassword !== validPassword)
             return res.status(401).json({ message: 'Mật khẩu không trùng nhau!' })
 
-        const hashedPassword = await bcrypt.hash(password, 12);
+        const hashedPassword = await bcrypt.hash(newPassword, 12);
 
-        user.password = newPassword.hashedPassword
+        user.password = hashedPassword
         await user.save()
 
-        res.status(200).json({ message: 'password is updated!' })
+        res.status(200).json({ message: 'Cập nhật mật khẩu thành công!' })
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
 }
 
 const updateUserInfo = async (req, res) => {
-    const userId = req.params
+    const { userId } = req.params
     const { fullname, email, phone, address } = req.body
 
     try {
-        const user = User.findOne(userId)
+        const user = await User.findOne({ userId })
         if(!user)
             return res.status(404).json({ message: 'Không tìm thấy người dùng!' })
 
@@ -48,4 +52,38 @@ const updateUserInfo = async (req, res) => {
     }
 }
 
-module.exports = { changeUserPassword, updateUserInfo }
+const getUserInfo = async (req, res) => {
+    const { userId } = req.params
+
+    try {
+        const user = await User.findOne({ userId })
+
+        if (!user)
+            return res.status(404).json({ message: 'Người dùng không tồn tại!' })
+
+        res.status(200).json({
+            user: user,
+            message: 'Lấy thành công thông tin user'
+        })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+const getAllUserInfo = async (req, res) => {
+    try {
+        const users = await User.find()
+
+        if (!users || users.length === 0)
+            return res.status(404).json({ message: 'Không có người dùng' })
+
+        res.status(200).json({
+            users: users,
+            message: 'Lấy danh sách người dùng thành công!'
+        })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+}
+
+module.exports = { changeUserPassword, updateUserInfo, getUserInfo, getAllUserInfo }
