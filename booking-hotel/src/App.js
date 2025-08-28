cd4b753f1a80ae55d057d95d3583a9066141f23d
Toann Lee <toanle291204@gmail.com>
2025-08-28 14:23:56 +0700

    update

--- booking-hotel/src/App.js
+++ booking-hotel/src/App.js
@@ -1,9 +1,12 @@
-import { Header } from "./component/Header/Header";
+import  {Header}  from "./component/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Banner } from "./component/Banner/Banner";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
