eb97b12fd6244975436d0ed56284543161498270
Nguyen-duy-viet-anh <vietanhnguyenduy1@gmail.com>
2025-08-28 11:00:25 +0700

    Footer

--- booking-hotel/src/App.js
+++ booking-hotel/src/App.js
@@ -1,9 +1,13 @@
+import Footer from "./component/Footer/Footer";
 import { Header } from "./component/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
