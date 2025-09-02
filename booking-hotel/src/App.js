94daf9340e35c38609d1f650cf8e02fe5aecc1bc
Toann Lee <toanle291204@gmail.com>
2025-09-02 12:44:21 +0700

    thêm HotelDetail

--- booking-hotel/src/App.js
+++ booking-hotel/src/App.js
@@ -1,16 +1,17 @@
 import Footer from "./component/Footer/Footer";
 import { Header } from "./component/Header/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import FeatureCard from "./component/FeatureCard/FeatureCard";
 import Banner from "./component/Banner/Banner";
 import HotelCard from "./component/HotelCard/HotelCard";
+import SearchPage from "./component/SearchPage/SearchPage";
+import HotelDetail from "./component/HotelDetail/HotelDetail";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Banner />
-      <FeatureCard />
-      <HotelCard />
+      <HotelDetail />
       <Footer />
     </div>
   );
