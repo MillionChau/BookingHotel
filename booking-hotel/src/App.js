4a6bde4a45255dd01d4e82f26d957063597c0cc9
Toann Lee <toanle291204@gmail.com>
2025-09-06 08:01:49 +0700

    Test AdminDashBoard

--- booking-hotel/src/App.js
+++ booking-hotel/src/App.js
@@ -5,15 +5,46 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import FeatureCard from "./component/FeatureCard/FeatureCard";
 import Banner from "./component/Banner/Banner";
 import HotelCard from "./component/HotelCard/HotelCard";
-import SearchPage from "./component/SearchPage/SearchPage";
 import HotelDetail from "./component/HotelDetail/HotelDetail";
+import SearchPage from "./component/SearchPage/SearchPage";
+import ProfilePage from "./component/Profile/Profile";
+import BookingHistory from "./component/BookingHistory/BookingHistory";
+import Dashboard from "./admin/DashBoard/DashBoard";
+import HotelManagement from "./admin/HotelManagement/HotelManagement";
+import RoomManager from "./admin/RoomManager/RoomManager";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./admin/SideBar/SideBar";
+import BookingManagement from "./admin/BookingManagement/BookingManagement";
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HotelDetail />
-      <Footer />
+    // <div className="App">
+    //   {/* <Header /> */}
+    //   {/* <SearchPage /> */}
+    //   {/* <HotelDetail /> */}
+    //   {/* <ProfilePage /> */}
+    //   {/* <BookingHistory /> */}
+    //   {/* <Footer /> */}
+    //   {/* <Dashboard /> */}
+    //   {/* <HotelManagement /> */}
+    //   {/* <RoomManager /> */}
+    // </div>
+    <BrowserRouter>
+    <div style={{ display: "flex" }}>
+      <Sidebar />
+      <div style={{ flex: 1, padding: "20px" }}>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/hotelmanagement" element={<HotelManagement />} />
+          <Route path="/rooms" element={<RoomManager />} />
+          <Route path="/bookings" element={<BookingManagement />} />
+          <Route path="/users" element={<RoomManager />} />
+          <Route path="/revenue" element={<RoomManager />} />
+          <Route path="/review" element={<RoomManager />} />
+        </Routes>
+      </div>
     </div>
+  </BrowserRouter>
   );
 }
 
